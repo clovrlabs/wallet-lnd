59b9e14167eca72cd744656b4760e812b58ae515
Roei Erez <roeierez@gmail.com>
2019-03-14 15:25:25 +0200

    server: better way to clean chainControl resources
    In this commit the location of where chain control services
    are stopped is shifted to be closer to the point they are started.
    Stopping of two services: "wallet" and "feeEstimator" that are started
    inside the "newChainControlFromConfig" was shifted from server.go to
    the cleanup function.
    
    In addition the chainView.Stop was also removed from the server.Stop as
    it is already handled by the router, where it is being started.

--- lnd.go
+++ lnd.go
@@ -375,7 +375,7 @@ func Main(lisCfg ListenerCfg) error {
 	// With the information parsed from the configuration, create valid
 	// instances of the pertinent interfaces required to operate the
 	// Lightning Network Daemon.
-	activeChainControl, err := newChainControlFromConfig(
+	activeChainControl, cleanup, err := newChainControlFromConfig(
 		cfg, chanDB, privateWalletPw, publicWalletPw,
 		walletInitParams.Birthday, walletInitParams.RecoveryWindow,
 		walletInitParams.Wallet, neutrinoCS,
@@ -385,6 +385,7 @@ func Main(lisCfg ListenerCfg) error {
 		ltndLog.Error(err)
 		return err
 	}
+	defer cleanup()
 
 	// Finally before we start the server, we'll register the "holy
 	// trinity" of interface for our current "home chain" with the active
