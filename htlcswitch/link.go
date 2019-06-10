1a4e07bd05b7e8baeaa4534c6d448a33dc78ef45
Roei Erez <roeierez@gmail.com>
2019-06-10 20:12:08 +0300

    implement backup events subscription.

--- htlcswitch/link.go
+++ htlcswitch/link.go
@@ -309,6 +309,8 @@ type ChannelLinkConfig struct {
 	// GetAliases is used by the link and switch to fetch the set of
 	// aliases for a given link.
 	GetAliases func(base lnwire.ShortChannelID) []lnwire.ShortChannelID
+
+	OnCommitmentRevoked func()
 }
 
 // shutdownReq contains an error channel that will be used by the channelLink
@@ -1968,6 +1970,7 @@ func (l *channelLink) handleUpstreamMsg(msg lnwire.Message) {
 			return
 		}
 		l.cfg.Peer.SendMessage(false, nextRevocation)
+		l.cfg.OnCommitmentRevoked()
 
 		// Notify the incoming htlcs of which the resolutions were
 		// locked in.
@@ -2094,6 +2097,7 @@ func (l *channelLink) handleUpstreamMsg(msg lnwire.Message) {
 		if l.failed {
 			return
 		}
+		l.cfg.OnCommitmentRevoked()
 
 		// The revocation window opened up. If there are pending local
 		// updates, try to update the commit tx. Pending updates could
