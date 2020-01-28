61ec144a296749d70f7b5583c971a5d1b5786505
Yaacov Akiba Slama <ya@slamail.org>
2020-01-28 17:04:36 +0200

    Add a configuration parameter to customize the remote channel reserve

---
+++ chanreserve_default.go
@@ -0,0 +1,12 @@
+// +build !chanreservedynamic
+
+package lnd
+
+import (
+	"github.com/btcsuite/btcutil"
+)
+
+func requiredRemoteChanReserve(chainCfg *chainConfig,
+	chanAmt, defaultReserve btcutil.Amount) (btcutil.Amount, error) {
+	return defaultReserve, nil
+}
