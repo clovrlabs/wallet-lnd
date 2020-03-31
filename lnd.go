getlipa/lnd-lspd

Allow signal re-use for usage as library.

lnd.go
@@ -97,6 +97,13 @@ var (
 // created in the top-level scope of a main method aren't executed if os.Exit()
 // is called.
 func Main() error {
+	
+	//Start the signal that is responsible for shutdown
+	if err := signal.Start(); err != nil {
+		ltndLog.Errorf("failed to start signal %v", err)
+		return err
+	}
+
 	// Load the configuration, and parse any command line options. This
 	// function will also set up logging properly.
 	loadedConfig, err := loadConfig()

signal/signal.go
@@ -6,8 +6,10 @@
 package signal
 
 import (
+	"errors"
 	"os"
 	"os/signal"
+	"sync/atomic"
 	"syscall"
 )
 
@@ -24,6 +26,8 @@ var (
 
 	// shutdownChannel is closed once the main interrupt handler exits.
 	shutdownChannel = make(chan struct{})
+
+	running int32
 )
 
 func init() {
@@ -34,8 +38,25 @@ func init() {
 		syscall.SIGTERM,
 		syscall.SIGQUIT,
 	}
-	signal.Notify(interruptChannel, signalsToCatch...)
+	signal.Notify(interruptChannel, signalsToCatch...)	
+}
+
+//Start starts the signal go routine and make it usable again.
+func Start() error {
+
+	if !atomic.CompareAndSwapInt32(&running, 0, 1) {
+		return errors.New("Signal already running")
+	}
+
+	// quit is closed when instructing the main interrupt handler to exit.
+	quit = make(chan struct{})
+
+	// shutdownChannel is closed once the main interrupt handler exits.
+	shutdownChannel = make(chan struct{})
+
 	go mainInterruptHandler()
+
+	return nil
 }
 
 // mainInterruptHandler listens for SIGINT (Ctrl+C) signals on the
@@ -64,6 +85,7 @@ func mainInterruptHandler() {
 		// Signal the main interrupt handler to exit, and stop accept
 		// post-facto requests.
 		close(quit)
+		atomic.StoreInt32(&running, 0)
 	}
 
 	for {
@@ -86,12 +108,7 @@ func mainInterruptHandler() {
 
 // Alive returns true if the main interrupt handler has not been killed.
 func Alive() bool {
-	select {
-	case <-quit:
-		return false
-	default:
-		return true
-	}
+	return atomic.LoadInt32(&running) == 1
 }
 
 // RequestShutdown initiates a graceful shutdown from the application.

clovrlabs/wallet-lnd

lnd.go
@@ -150,8 +150,11 @@ type rpcListeners func() ([]*ListenerWithSignal, func(), error)
 // created in the top-level scope of a main method aren't executed if os.Exit()
 // is called.
 func Main(lisCfg ListenerCfg) error {
-	// Hook interceptor for os signals.
-	signal.Intercept()
+	//Start the signal that is responsible for shutdown
+	if err := signal.Intercept(); err != nil {
+		ltndLog.Errorf("failed to start signal %v", err)
+		return err
+	}
 
 	// Load the configuration, and parse any command line options. This
 	// function will also set up logging properly.

signal/signal.go
@@ -6,8 +6,10 @@
 package signal
 
 import (
+	"errors"
 	"os"
 	"os/signal"
+	"sync/atomic"
 	"syscall"
 )
 
@@ -24,10 +26,22 @@ var (
 
 	// shutdownChannel is closed once the main interrupt handler exits.
 	shutdownChannel = make(chan struct{})
+
+	running int32
 )
 
 // Intercept starts the interception of interrupt signals.
-func Intercept() {
+func Intercept() error {
+	if !atomic.CompareAndSwapInt32(&running, 0, 1) {
+		return errors.New("Signal already running")
+	}
+
+	// quit is closed when instructing the main interrupt handler to exit.
+	quit = make(chan struct{})
+
+	// shutdownChannel is closed once the main interrupt handler exits.
+	shutdownChannel = make(chan struct{})
+
 	signalsToCatch := []os.Signal{
 		os.Interrupt,
 		os.Kill,
@@ -37,6 +51,8 @@ func Intercept() {
 	}
 	signal.Notify(interruptChannel, signalsToCatch...)
 	go mainInterruptHandler()
+
+	return nil
 }
 
 // mainInterruptHandler listens for SIGINT (Ctrl+C) signals on the
@@ -65,6 +81,7 @@ func mainInterruptHandler() {
 		// Signal the main interrupt handler to exit, and stop accept
 		// post-facto requests.
 		close(quit)
+		atomic.StoreInt32(&running, 0)
 	}
 
 	for {
@@ -87,12 +104,7 @@ func mainInterruptHandler() {
 
 // Alive returns true if the main interrupt handler has not been killed.
 func Alive() bool {
-	select {
-	case <-quit:
-		return false
-	default:
-		return true
-	}
+	return atomic.LoadInt32(&running) == 1
 }
 
 // RequestShutdown initiates a graceful shutdown from the application.