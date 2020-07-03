8a7aadfebd65d9f29815cea3309298c7c51853f3
Yaacov Akiba Slama <ya@slamail.org>
2020-07-03 18:40:34 +0300

    Add OnionBlob and OutgoingAmount parameters to the interceptor

--- lnrpc/routerrpc/forward_interceptor.go
+++ lnrpc/routerrpc/forward_interceptor.go
@@ -163,6 +163,7 @@ func (r *forwardInterceptor) holdAndforwardToClient(
 		IncomingAmountMsat:      uint64(htlc.IncomingAmount),
 		IncomingExpiry:          htlc.IncomingExpiry,
 		CustomRecords:           htlc.CustomRecords,
+		OnionBlob:               htlc.OnionBlob[:],
 	}
 
 	return r.stream.Send(interceptionRequest)
@@ -185,7 +186,9 @@ func (r *forwardInterceptor) resolveFromClient(
 
 	switch in.Action {
 	case ResolveHoldForwardAction_RESUME:
-		return interceptedForward.Resume()
+		var onionBlob [lnwire.OnionPacketSize]byte
+		copy(onionBlob[:], in.OnionBlob)
+		return interceptedForward.Resume(lnwire.MilliSatoshi(in.OutgoingAmountMsat), onionBlob)
 	case ResolveHoldForwardAction_FAIL:
 		return interceptedForward.Fail()
 	case ResolveHoldForwardAction_SETTLE:
@@ -217,7 +220,7 @@ func (r *forwardInterceptor) onDisconnect() {
 
 	log.Infof("RPC interceptor disconnected, resolving held packets")
 	for key, forward := range r.holdForwards {
-		if err := forward.Resume(); err != nil {
+		if err := forward.Resume(forward.Packet().OutgoingAmount, forward.Packet().OnionBlob); err != nil {
 			log.Errorf("failed to resume hold forward %v", err)
 		}
 		delete(r.holdForwards, key)
