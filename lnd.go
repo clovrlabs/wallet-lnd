getlipa/lnd-lspd

Allow signal re-use for usage as library.

lnd.go
@@ -97,6 +97,13 @@ var (
 // created in the top-level scope of a main method aren't executed if os.Exit()
 // is called.
 func Main() error {
+	
+	//Start the signal that is responsible for shutdown
+	if err := signal.Start(); err != nil {
+		ltndLog.Errorf("failed to start signal %v", err)
+		return err
+	}
+
 	// Load the configuration, and parse any command line options. This
 	// function will also set up logging properly.
 	loadedConfig, err := loadConfig()

clovrlabs/wallet-lnd

lnd.go
@@ -188,6 +188,11 @@ type rpcListeners func() ([]*ListenerWithSignal, func(), error)
 // This function starts all main system components then blocks until a signal
 // is received on the shutdownChan at which point everything is shut down again.
 func Main(cfg *Config, lisCfg ListenerCfg, shutdownChan <-chan struct{}) error {
+	//Start the signal that is responsible for shutdown
+	if err := signal.Intercept(); err != nil {
+		ltndLog.Errorf("failed to start signal %v", err)
+		return err
+	}
 	defer func() {
 		ltndLog.Info("Shutdown complete")
 		err := cfg.LogWriter.Close()