2908dccde2b73bb14f1a961f46499057af89284a
Roei Erez <roeierez@gmail.com>
2020-07-19 13:01:59 +0300

    lnwire+invoices: Hold unconfirmed channel payments
    This commit intorduces some preparations needed for allowing skipping
    funding tx confirmation and allowing payments on unconfirmed channels.
    Since the risk in non confirmed channels is in accepting payments, the
    logic was chaned to hold all payments received in unconfirmed channels
    requiring the user to explicitly approve them.

--- invoices/update.go
+++ invoices/update.go
@@ -136,9 +136,10 @@ func updateMpp(ctx *invoiceUpdateCtx,
 		return nil, ctx.failRes(ResultHtlcSetTotalTooLow), nil
 	}
 
+	hodlInvoice := inv.HodlInvoice || ctx.circuitKey.ChanID.IsFake()
 	// Check whether total amt matches other htlcs in the set.
 	var newSetTotal lnwire.MilliSatoshi
-	for _, htlc := range inv.Htlcs {
+	for circuitKey, htlc := range inv.Htlcs {
 		// Only consider accepted mpp htlcs. It is possible that there
 		// are htlcs registered in the invoice database that previously
 		// timed out and are in the canceled state now.
@@ -150,6 +151,9 @@ func updateMpp(ctx *invoiceUpdateCtx,
 			return nil, ctx.failRes(ResultHtlcSetTotalMismatch), nil
 		}
 
+		if circuitKey.ChanID.IsFake() {
+			hodlInvoice = true
+		}
 		newSetTotal += htlc.Amt
 	}
 
@@ -187,7 +191,7 @@ func updateMpp(ctx *invoiceUpdateCtx,
 
 	// Check to see if we can settle or this is an hold invoice and
 	// we need to wait for the preimage.
-	if inv.HodlInvoice {
+	if hodlInvoice {
 		update.State = &channeldb.InvoiceStateUpdateDesc{
 			NewState: channeldb.ContractAccepted,
 		}
@@ -274,7 +278,7 @@ func updateLegacy(ctx *invoiceUpdateCtx,
 
 	// Check to see if we can settle or this is an hold invoice and we need
 	// to wait for the preimage.
-	if inv.HodlInvoice {
+	if inv.HodlInvoice || ctx.circuitKey.ChanID.IsFake() {
 		update.State = &channeldb.InvoiceStateUpdateDesc{
 			NewState: channeldb.ContractAccepted,
 		}
