0c3b159982d81afda1a99374cbdc2ed958223ac8
Roei Erez <roeierez@Roeis-iMac.local>
2020-09-30 12:38:12 +0300

    advertise zero-conf support with feature bit.

--- lnwire/features.go
+++ lnwire/features.go
@@ -138,6 +138,15 @@ const (
 	// of a payment supports accepts spontaneous payments, i.e.
 	// sender-generated preimages according to BOLT XX.
 	AMPOptional FeatureBit = 31
+	// SkipFundingConfirmationRequired is a required feature bit that signals that the node
+	// supports initiated channels to be made operational before funding transaction is
+	// confirmed on chain
+	SkipFundingConfirmationRequired FeatureBit = 1338
+
+	// SkipFundingConfirmationOptional is a required feature bit that signals that the node
+	// supports initiated channels to be made operational before funding transaction is
+	// confirmed on chain
+	SkipFundingConfirmationOptional FeatureBit = 1339
 
 	// maxAllowedSize is a maximum allowed size of feature vector.
 	//
@@ -161,29 +170,31 @@ func (b FeatureBit) IsRequired() bool {
 // feature bits must be assigned a name in this mapping, and feature bit pairs
 // must be assigned together for correct behavior.
 var Features = map[FeatureBit]string{
-	DataLossProtectRequired:       "data-loss-protect",
-	DataLossProtectOptional:       "data-loss-protect",
-	InitialRoutingSync:            "initial-routing-sync",
-	UpfrontShutdownScriptRequired: "upfront-shutdown-script",
-	UpfrontShutdownScriptOptional: "upfront-shutdown-script",
-	GossipQueriesRequired:         "gossip-queries",
-	GossipQueriesOptional:         "gossip-queries",
-	TLVOnionPayloadRequired:       "tlv-onion",
-	TLVOnionPayloadOptional:       "tlv-onion",
-	StaticRemoteKeyOptional:       "static-remote-key",
-	StaticRemoteKeyRequired:       "static-remote-key",
-	PaymentAddrOptional:           "payment-addr",
-	PaymentAddrRequired:           "payment-addr",
-	MPPOptional:                   "multi-path-payments",
-	MPPRequired:                   "multi-path-payments",
-	AnchorsRequired:               "anchor-commitments",
-	AnchorsOptional:               "anchor-commitments",
-	AnchorsZeroFeeHtlcTxRequired:  "anchors-zero-fee-htlc-tx",
-	AnchorsZeroFeeHtlcTxOptional:  "anchors-zero-fee-htlc-tx",
-	WumboChannelsRequired:         "wumbo-channels",
-	WumboChannelsOptional:         "wumbo-channels",
-	AMPRequired:                   "amp",
-	AMPOptional:                   "amp",
+	DataLossProtectRequired:         "data-loss-protect",
+	DataLossProtectOptional:         "data-loss-protect",
+	InitialRoutingSync:              "initial-routing-sync",
+	UpfrontShutdownScriptRequired:   "upfront-shutdown-script",
+	UpfrontShutdownScriptOptional:   "upfront-shutdown-script",
+	GossipQueriesRequired:           "gossip-queries",
+	GossipQueriesOptional:           "gossip-queries",
+	TLVOnionPayloadRequired:         "tlv-onion",
+	TLVOnionPayloadOptional:         "tlv-onion",
+	StaticRemoteKeyOptional:         "static-remote-key",
+	StaticRemoteKeyRequired:         "static-remote-key",
+	PaymentAddrOptional:             "payment-addr",
+	PaymentAddrRequired:             "payment-addr",
+	MPPOptional:                     "multi-path-payments",
+	MPPRequired:                     "multi-path-payments",
+	AnchorsRequired:                 "anchor-commitments",
+	AnchorsOptional:                 "anchor-commitments",
+	AnchorsZeroFeeHtlcTxRequired:    "anchors-zero-fee-htlc-tx",
+	AnchorsZeroFeeHtlcTxOptional:    "anchors-zero-fee-htlc-tx",
+	WumboChannelsRequired:           "wumbo-channels",
+	WumboChannelsOptional:           "wumbo-channels",
+	AMPRequired:                     "amp",
+	AMPOptional:                     "amp",
+	SkipFundingConfirmationOptional: "skip-funding-confirmation",
+	SkipFundingConfirmationRequired: "skip-funding-confirmation",
 }
 
 // RawFeatureVector represents a set of feature bits as defined in BOLT-09.  A
