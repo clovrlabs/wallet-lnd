54512554f0c5e4901f15b2618b4f6a9dc0ffd4fc
Roei Erez <roeierez@gmail.com>
2022-07-13 16:26:45 +0300

    allow channel reserve bellow dust.

--- lnwallet/reservation.go
+++ lnwallet/reservation.go
@@ -471,12 +471,6 @@ func (r *ChannelReservation) CommitConstraints(c *channeldb.ChannelConstraints,
 		return ErrCsvDelayTooLarge(c.CsvDelay, maxLocalCSVDelay)
 	}
 
-	// The channel reserve should always be greater or equal to the dust
-	// limit. The reservation request should be denied if otherwise.
-	if c.DustLimit > c.ChanReserve {
-		return ErrChanReserveTooSmall(c.ChanReserve, c.DustLimit)
-	}
-
 	// Validate against the maximum-sized witness script dust limit, and
 	// also ensure that the DustLimit is not too large.
 	maxWitnessLimit := DustLimitForSize(input.UnknownWitnessSize)
